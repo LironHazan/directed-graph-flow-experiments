3dfa7666c5e8a24a499d93a01110a60b8c2b3fb8
lironh <lironenk@gmail.com>
2018-12-09 23:10:49 +0200

    :hammer: refactor - functional approach

--- src/index.js
+++ src/index.js
@@ -1,46 +1,26 @@
 import * as d3 from "d3";
 import "d3-selection-multi";
-import {mockedData} from './utils';
-
-let svg = d3.select("svg"),
-    node,
-    edgepaths,
-    edgelabels,
+import {mockedData, 
+    clearView, 
+    removeLinksRelations, 
+    ticked, 
     dragended,
-    text,
-    link;
+    initDefinitions,
+    forceSimulation
+} from './utils';
 
+const svg = d3.select("svg");
 const width = +svg.attr("width");
 const height = +svg.attr("height");
 
-svg.append('defs')
-    .append('marker')
-    .attrs({'id':'arrowhead',
-        'viewBox':'-0 -5 10 10',
-        'refX':34,
-        'refY':0,
-        'orient':'auto',
-        'markerWidth':8,
-        'markerHeight':8,
-        'xoverflow':'visible'})
-    .append('svg:path')
-    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-    .attr('fill', '#999')
-    .style('stroke','none');
+export const render = (data) => {
+    
+    let { links, nodes } = data;
+    clearView(svg); // removes everything! 
+    initDefinitions(svg);
+    const simulation = forceSimulation(d3, {width,height});
 
-let simulation = d3.forceSimulation()
-    .force("link", 
-    d3.forceLink()
-    .id(function (d) {return d.id;})
-    .distance(200)
-    .strength(2))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
-
-render(mockedData.links, mockedData.nodes);
-
-function render(links, nodes) {
-    link = svg.selectAll(".link")
+    const link = svg.selectAll(".link")
         .data(links)
         .enter()
         .append("line")
@@ -50,7 +30,7 @@ function render(links, nodes) {
     link.append("title")
         .text(function (d) {return d.type;});
 
-    edgepaths = svg.selectAll(".edgepath")
+    const edgepaths = svg.selectAll(".edgepath")
         .data(links)
         .enter()
         .append('path')
@@ -62,7 +42,7 @@ function render(links, nodes) {
         })
         .style("pointer-events", "none");
 
-    edgelabels = svg.selectAll(".edgelabel")
+    const edgelabels = svg.selectAll(".edgelabel")
         .data(links)
         .enter()
         .append('text')
@@ -81,22 +61,23 @@ function render(links, nodes) {
         .attr("startOffset", "50%")
         .text(function (d) {return d.type});
 
-    node = svg.selectAll(".node")
+    const node = svg.selectAll(".node")
         .data(nodes)
         .enter()
         .append("g")
         .attr("class", "node")
         .call(d3.drag()
-               .on("start", dragstarted)
-               .on("drag", dragged)
-               .on("end", dragended)
+               .on("start", (d) => dragended(d3, d, simulation))
+               .on("drag", function dragged(d) {
+                d.fx = d3.event.x;
+                d.fy = d3.event.y;
+            })
+               .on("end", (d) => dragended(d3, d, simulation))
          );
-        
 
     node.append('circle').attr("r", 40); //radius
     svg.selectAll('circle')
-    .on('click', function (selected) { // arrow function will produce this = undefined
-       // console.log(selected);
+    .on('click', function () { // arrow function will produce this = undefined
        d3.selectAll('circle')
        .style("fill", "lightgray");
        d3.select(this)
@@ -105,22 +86,35 @@ function render(links, nodes) {
      .on('mouseover', function () {
         d3.selectAll('circle')
         .style("stroke", "black");
+        
         d3.select(this)
         .style("stroke", "green");
     });
      
-    text = node.append("text")
+    // appand trash icons to all nodes
+    node.append('svg:foreignObject')
+    .attr("class", "delete-icon")
+    .html('<i class="fa fa-trash"></i>');
+
+    svg.selectAll('.delete-icon')
+    .on('click', function ({id}) {
+        links = removeLinksRelations(links, id);
+        nodes = nodes.filter(node => id !== node.id);
+        render({ links, nodes });
+      })
+
+    const nodeText = node.append("text")
         .attr("dy", -3)
         .attr("y", -25);
 
-    text.selectAll("tspan.text")
+    nodeText.selectAll("tspan.text")
     .data((d) =>  d.name.split(" "))
     .enter()
     .append("tspan")
     .attr("class", "text")
     .text(d => d)
     .attr("x", -10)
-     .attr("dx", 10)
+    .attr("dx", 10)
     .attr("dy", 22);
 
     node.append("title")
@@ -128,47 +122,11 @@ function render(links, nodes) {
 
     simulation
         .nodes(nodes)
-        .on("tick", ticked);
+        .on("tick", () => { ticked(link, node, edgepaths, edgelabels)});
 
     simulation.force("link")
         .links(links);
 }
 
-function ticked() {
-    link
-        .attr("x1", function (d) {return d.source.x;})
-        .attr("y1", function (d) {return d.source.y;})
-        .attr("x2", function (d) {return d.target.x;})
-        .attr("y2", function (d) {return d.target.y;});
+render(mockedData);
 
-    node.attr("transform", 
-    function (d) {return "translate(" + d.x + ", " + d.y + ")";});
-
-    edgepaths.attr('d', function (d) {
-        return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
-    });
-
-    edgelabels.attr('transform', function (d) {
-        if (d.target.x < d.source.x) {
-            let bbox = this.getBBox();
-
-            let rx = bbox.x + bbox.width / 2;
-            let ry = bbox.y + bbox.height / 2;
-            return 'rotate(180 ' + rx + ' ' + ry + ')';
-        }
-        else {
-            return 'rotate(0)';
-        }
-    });
-}
-
-function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
-    d.fx = d.x;
-    d.fy = d.y;
-}
-
-function dragged(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-}
--- src/utils.js
+++ src/utils.js
@@ -4,50 +4,128 @@ d3.selectAll(element)
 return d3;
 }
 
+export const clearView = (svg) => svg.selectAll("*").remove();
+
 export const mockedData = {
     "nodes": [
       {
         "name": "Node1 one",
         "label": "Node1",
-        "id": 1
+        "id": 1,
+        "links": [1, 2]
       },
       {
         "name": "Node2",
         "label": "Node2",
-        "id": 2
+        "id": 2,
+        "links": [2, 3]
       },
       {
         "name": "Node3",
         "label": "Node3",
-        "id": 3
+        "id": 3,
+        "links": [2, 3, 4]
       },
       {
         "name": "Node4",
         "label": "Node4",
-        "id": 4
+        "id": 4,
+        "links": [4]
       }
     ],
     "links": [
       {
+        "id": 1,
         "source": 1,
         "target": 2,
         "type": "Foo",
         "since": 2010
       },
       {
+        "id": 2,
         "source": 1,
         "target": 3,
         "type": "Bar"
       },
       {
+        "id": 3,
         "source": 2,
         "target": 3,
         "type": "Baz"
       },
       {
+        "id": 4,
         "source": 3,
         "target": 4,
         "type": "Foo2"
       }
     ]
   }
+
+export const removeLinksRelations = (links, nodeId) => {
+  return links.reduce((_links, link) => {
+    if (link.source.id !== nodeId && link.target.id !== nodeId) {
+        _links.push(link);
+    }
+    return _links;
+}, []);
+}
+
+export const ticked = (link, node, edgepaths, edgelabels) => {
+  link
+      .attr("x1", function (d) {return d.source.x;})
+      .attr("y1", function (d) {return d.source.y;})
+      .attr("x2", function (d) {return d.target.x;})
+      .attr("y2", function (d) {return d.target.y;});
+
+  node.attr("transform", 
+  function (d) {return "translate(" + d.x + ", " + d.y + ")";});
+
+  edgepaths.attr('d', function (d) {
+      return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
+  });
+
+  edgelabels.attr('transform', function (d) {
+      if (d.target.x < d.source.x) {
+          let bbox = this.getBBox();
+
+          let rx = bbox.x + bbox.width / 2;
+          let ry = bbox.y + bbox.height / 2;
+          return 'rotate(180 ' + rx + ' ' + ry + ')';
+      }
+      else {
+          return 'rotate(0)';
+      }
+  });
+}
+
+export const dragended = (d3, d, simulation) => {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+export const initDefinitions = (svg) =>
+    svg.append('defs')
+    .append('marker')
+    .attrs({'id':'arrowhead',
+        'viewBox':'-0 -5 10 10',
+        'refX':34,
+        'refY':0,
+        'orient':'auto',
+        'markerWidth':8,
+        'markerHeight':8,
+        'xoverflow':'visible'})
+    .append('svg:path')
+    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+    .attr('fill', '#999')
+    .style('stroke','none');
+
+export const forceSimulation = (d3, {width, height}) => d3.forceSimulation()
+.force("link", 
+d3.forceLink()
+.id(function (d) {return d.id;})
+.distance(200)
+.strength(2))
+.force("charge", d3.forceManyBody())
+.force("center", d3.forceCenter(width / 2, height / 2));
