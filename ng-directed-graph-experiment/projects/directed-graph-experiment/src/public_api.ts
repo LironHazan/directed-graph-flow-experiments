9140e83c2f522e5a660b296b0096d8d43650d4a7
liron.hazan <liron.hazan@cybereason.com>
2019-06-14 08:48:08 +0300

    removed angular project

--- ng-directed-graph-experiment/projects/directed-graph-experiment/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of directed-graph-experiment
- */
-
-export * from './lib/directed-graph-experiment.service';
-export * from './lib/directed-graph-experiment.component';
-export * from './lib/directed-graph-experiment.module';
